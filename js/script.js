1a484105311f6a642b9dbb2df0886c563b136f17
EgeaHdp <xtkjegeahadiputra@gmail.com>
2025-10-11 00:51:22 +0800

    Refactor HTML structure and enhance JavaScript functionality for portfolio

---
+++ js/script.js
@@ -0,0 +1,23 @@
+console.log("he;llo world");
+
+greet();
+
+
+function greet() {
+    const name = prompt("Please enter your name:");
+    document.getElementById("welcome-speech").innerHTML = name;
+}
+
+function validateForm() {
+    /// ini adalah function untuk validasi form
+    let name = document.getElementById("name-input").value;
+    let email = document.getElementById("email-input").value;
+    let message = document.getElementById("message-input").value;
+
+    /// cek apakah nama kosong
+    if (name === "" || email === "" || message ===  "" ) {
+        alert("Please fill in all fields.");
+        return
+    }
+    alert ("Thank you for your message, " + name + "!");
+}
